516cc718a74825cabfd68690c845b0b1112d115f
Muhammad Hamza <muhammad.hamza@ai.local>
2026-05-14 02:26:33 -0700

    update on the server

--- result-app/server.js
+++ result-app/server.js
@@ -1,14 +1,28 @@
-const express = require('express'), { Pool } = require('pg'), app = express();
-const pool = new Pool({ connectionString: 'postgres://postgres:postgrespassword@db:5432/postgres' });
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+
+const pool = new Pool({
+    connectionString: 'postgres://postgres:postgrespassword@db:5432/postgres'
+});
 
 app.use(express.static(__dirname + '/views'));
+
 app.get('/data', async (req, res) => {
     try {
         const client = await pool.connect();
-        const result = await client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote');
+
+        const result = await client.query(
+            'SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote'
+        );
+
         client.release();
+
         res.json(result.rows);
-    } catch (err) { res.status(500).send(err.toString()); }
+    } catch (err) {
+        res.status(500).send(err.toString());
+    }
 });
 
 app.listen(80, () => console.log('Listening on port 80'));
